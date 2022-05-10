00a639715c2fca8802c406b36d576f59c1c67e4b
Angga Aditya <72496912+GungdeAngga@users.noreply.github.com>
2022-05-10 22:54:14 +0800

    Add files via upload

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,36 +1,57 @@
 <template>
 
   <v-app class="homePage">
-<div>
-    <v-app-bar
-      color="#4E45CE"
-      dense
-      dark
-      style="height: 137px;"
-    >
+    <div>
+        <v-app-bar
+          color="#4E45CE"
+          dense
+          dark
+          style="height: 137px;"
+        >
 
-      <v-toolbar-title>Corsair</v-toolbar-title>
+          <v-toolbar-title><strong>Corsair</strong></v-toolbar-title>
 
-      <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-      <v-btn text>
-        support
-      </v-btn>
+          <v-btn text>
+            support
+          </v-btn>
 
-      <v-btn text>
-        angga aditya
-      </v-btn>
-    </v-app-bar>
-</div>
+          <v-btn text>
+            angga aditya
+          </v-btn>
+        </v-app-bar>
+    </div>
+
 
 
 <div style="background-color:#2C2D30; height:100%; ">
-  <div class="jmlhUang">
-  <h5>Account Balance</h5>
-  <h2>Rp 10.0000.000</h2>
-  <!-- note : buat tulisan di footer sama header ke tengah -->
-  </div>
+
+    <div class="jmlhUang">
+    <h5>Account Balance</h5>
+    <h2>Rp 10.0000.000</h2>
+    <!-- note : buat tulisan di footer sama header ke tengah -->
+        <div class="threeButton">
+            <v-btn depressed 
+            color="#4E45CE">
+              send
+            </v-btn>
+
+            <v-btn depressed
+            color="#4E45CE">
+              pulsa
+            </v-btn>
+
+            <v-btn depressed
+            color="#4E45CE">
+              withdraw
+            </v-btn>
+            <!-- buat kotak nya sama -->
+        </div>
+    </div>
+
 </div>
+
 
 
 <div style="position:absolute; bottom:0px; width:100%; ">
@@ -82,6 +103,13 @@ export default {
   padding: 40px;
   width: 600px;
   height: 130px;
+}
+
+.threeButton{
+  display: flex;
+  margin: 10%;
+  column-count: 3;
+  column-gap: 50px;
 }
 
 h5{
